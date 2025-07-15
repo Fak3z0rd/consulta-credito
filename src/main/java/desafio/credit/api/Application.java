f7dd8705adf9989b9b3fb3d418447e64d0655c78
Vinicius Meireles <vinicius.meireles@euconectopessoas.com.br>
2025-07-14 22:41:05 -0300

    classe principal + properties + banco de dados

--- src/main/java/desafio/credit/api/Application.java
+++ src/main/java/desafio/credit/api/Application.java
@@ -2,8 +2,14 @@ package desafio.credit.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"desafio.credit"})
+@EnableJpaRepositories(basePackages = {"desafio.credit.domain.repository"})
+@EntityScan(basePackages = {"desafio.credit.domain.model"})
 public class Application {
 
 	public static void main(String[] args) {
