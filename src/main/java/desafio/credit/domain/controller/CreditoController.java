efd862eefd89ac384aaedf7fd522ca6afb6150fd
Vinicius Meireles <vinicius.meireles@euconectopessoas.com.br>
2025-07-14 22:39:25 -0300

    cria controller

---
+++ src/main/java/desafio/credit/domain/controller/CreditoController.java
@@ -0,0 +1,40 @@
+package desafio.credit.domain.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import desafio.credit.config.KafkaProducer;
+import desafio.credit.domain.service.CreditoService;
+import desafio.credit.domain.model.Credito;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/creditos")
+public class CreditoController {
+
+    private final CreditoService creditoService;
+
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    public CreditoController(CreditoService creditoService) {
+        this.creditoService = creditoService;
+    }
+
+    @GetMapping("/nfse/{nfse}")
+    public ResponseEntity<Credito> buscarPorNfse(@PathVariable String nfse) {
+        kafkaProducer.sendMessage("consulta-nfse", nfse);
+        return creditoService.buscarPorNfse(nfse)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/numero/{numeroCredito}")
+    public ResponseEntity<List<Credito>> buscarPorNumeroCredito(@PathVariable String numeroCredito) {
+        kafkaProducer.sendMessage("consulta-numero-credito", numeroCredito);
+        List<Credito> creditos = creditoService.buscarPorNumeroCredito(numeroCredito);
+        return ResponseEntity.ok(creditos);
+    }
+}
